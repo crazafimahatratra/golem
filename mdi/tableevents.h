4c10c92a16592e6eb189e8c2cd7264fdf0c64e9b
Administrator <admin@example.com>
2019-01-24 14:28:44 +0300

    Documentation for tableevents

--- mdi/tableevents.h
+++ mdi/tableevents.h
@@ -6,19 +6,35 @@
 #include <QMimeData>
 #include <QDropEvent>
 
+/**
+ * @brief Base class for the table widget containing events
+ */
 class TableEvents : public QTableWidget
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor
+     * @param parent : the parent widget
+     */
     explicit TableEvents(QWidget *parent = nullptr);
 
 signals:
+    /**
+     * @brief raised when a task is dropped on the widget
+     * @param id : the id of the task. Task::id
+     */
     void taskReceived(int id);
 
 public slots:
 
     // QTableWidget interface
 protected:
+    /**
+     * @brief dropMimeData called when something is dropped upon the widget
+     * @param data : the data :)
+     * @return boolean saying if data was accepted or not.
+     */
     bool dropMimeData(int, int, const QMimeData *data, Qt::DropAction);
 };
 
